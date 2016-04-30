3a436216be4e408610e207416f278ff07267c52f
yohira0616 <yukito.sys.so@gmail.com>
2016-04-30 16:11:24 +0900

    何故かbower.jsonをコミットし忘れていた

--- common/connection.js
+++ common/connection.js
@@ -1,3 +1,3 @@
 var Sequelize = require('sequelize');
 
-module.exports = new Sequelize('postgres://postgres:postgress@localhost:5432/sample_task_app');
+module.exports = new Sequelize('postgres://postgres:postgres@localhost:5432/sample_task_app');
