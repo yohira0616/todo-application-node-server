816699865a56d45ace86cb249263a5bd11c3737f
yohira0616 <yukito.sys.so@gmail.com>
2016-04-30 17:34:47 +0900

    CORS制約の解除

--- app.js
+++ app.js
@@ -27,11 +27,20 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/task', task);
 
+//enable cross-origin
+/* FIXME not working now...
+app.all('/task/*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  return next();
+});*/
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -39,23 +48,23 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
--- routes/task.index.js
+++ routes/task.index.js
@@ -4,30 +4,39 @@ var router = express.Router();
 var TaskRepository = require('../repository/task.repository');
 var Task = require('../models/task');
 
+function corsEnable(res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+}
+
 router.get('/all', function (req, res, next) {
-    Task.findAll().then(function (result) {
-        console.log(result);
-        res.send(result);
-    });
+  corsEnable(res);
+
+  Task.findAll().then(function (result) {
+    console.log(result);
+    res.send(result);
+  });
 });
 
 router.all('/new', function (req, res, next) {
-    console.log(req.body);
-    var param = req.body;
-    Task.create(param).then(function () {
-        res.send('done');
-    });
+  corsEnable(res);
+  var param = req.body;
+  Task.create(param).then(function () {
+    res.send('done');
+  });
 });
 
 router.all('/done', function (req, res, next) {
-    var param = req.body;
-    Task.destroy({
-        where: {
-            id: param.id
-        }
-    }).then(function () {
-        res.send('done');
-    });
+  corsEnable(res);
+  var param = req.body;
+  Task.destroy({
+    where: {
+      id: param.id
+    }
+  }).then(function () {
+    res.send('done');
+  });
 });
 
 
